1fe9fa90c5d7aa9f70495a8659d73eeff5e80315
MirazZim <miraz.zim.38@gmail.com>
2025-01-08 14:01:09 +0600

    Why we should use loader instead of useEffect. Because it is less problamatic and easy to load data

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
     return (
         <div>
             <Header></Header>
-            
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,6 +1,12 @@
+import { useLoaderData } from "react-router-dom";
+
 const User = () => {
+const users = useLoaderData();
+console.log(users)
+
     return (
         <div>
+            <h2>Our Users: {users.length}</h2>
             <h3>This is for the Users</h3>
         </div>
     );
